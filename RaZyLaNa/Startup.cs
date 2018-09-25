1ee4dd29e3865ab478a1231bbf113a59ba4ec700
EeNiArT <34338278+EeNiArT@users.noreply.github.com>
2018-09-25 23:11:35 +0500

    Add project files.

---
+++ RaZyLaNa/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RaZyLaNa.Startup))]
+namespace RaZyLaNa
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
